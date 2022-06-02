33d987da94aed06a0d37c12faf2ad2fa12ed5af1
Dan Gabriel Lettac <dan.lettac@gmail.com>
2022-06-02 23:59:20 +0800

    added a draw feature

--- Form1.cs
+++ Form1.cs
@@ -73,14 +73,14 @@ namespace Tic_Tac_Toe
         private void Check()
         {
             if (
-        (Btn1.Text == "X" && Btn2.Text == "X" && Btn3.Text == "X") ||//Horizontal
-        (Btn4.Text == "X" && Btn5.Text == "X" && Btn6.Text == "X") ||
-        (Btn7.Text == "X" && Btn8.Text == "X" && Btn9.Text == "X") ||
-        (Btn1.Text == "X" && Btn4.Text == "X" && Btn7.Text == "X") ||//Vertical)
-        (Btn2.Text == "X" && Btn5.Text == "X" && Btn8.Text == "X") ||
-        (Btn3.Text == "X" && Btn6.Text == "X" && Btn9.Text == "X") ||
-        (Btn1.Text == "X" && Btn5.Text == "X" && Btn9.Text == "X") ||//Diagonal
-        (Btn3.Text == "X" && Btn5.Text == "X" && Btn7.Text == "X"))
+            (Btn1.Text == "X" && Btn2.Text == "X" && Btn3.Text == "X") ||//Horizontal
+            (Btn4.Text == "X" && Btn5.Text == "X" && Btn6.Text == "X") ||
+            (Btn7.Text == "X" && Btn8.Text == "X" && Btn9.Text == "X") ||
+            (Btn1.Text == "X" && Btn4.Text == "X" && Btn7.Text == "X") ||//Vertical)
+            (Btn2.Text == "X" && Btn5.Text == "X" && Btn8.Text == "X") ||
+            (Btn3.Text == "X" && Btn6.Text == "X" && Btn9.Text == "X") ||
+            (Btn1.Text == "X" && Btn5.Text == "X" && Btn9.Text == "X") ||//Diagonal
+            (Btn3.Text == "X" && Btn5.Text == "X" && Btn7.Text == "X"))
             {
                 MessageBox.Show("Player wins");
                 playerWins++;
@@ -89,14 +89,14 @@ namespace Tic_Tac_Toe
 
             }
             else if (
-        (Btn1.Text == "O" && Btn2.Text == "O" && Btn3.Text == "O") ||//Horizontal
-        (Btn4.Text == "O" && Btn5.Text == "O" && Btn6.Text == "O") ||
-        (Btn7.Text == "O" && Btn8.Text == "O" && Btn9.Text == "O") ||
-        (Btn1.Text == "O" && Btn4.Text == "O" && Btn7.Text == "O") ||//Vertical
-        (Btn2.Text == "O" && Btn5.Text == "O" && Btn8.Text == "O") ||
-        (Btn3.Text == "O" && Btn6.Text == "O" && Btn9.Text == "O") ||
-        (Btn1.Text == "O" && Btn5.Text == "O" && Btn9.Text == "O") || //Diagonal
-        (Btn3.Text == "O" && Btn5.Text == "O" && Btn7.Text == "O"))
+            (Btn1.Text == "O" && Btn2.Text == "O" && Btn3.Text == "O") ||//Horizontal
+            (Btn4.Text == "O" && Btn5.Text == "O" && Btn6.Text == "O") ||
+            (Btn7.Text == "O" && Btn8.Text == "O" && Btn9.Text == "O") ||
+            (Btn1.Text == "O" && Btn4.Text == "O" && Btn7.Text == "O") ||//Vertical
+            (Btn2.Text == "O" && Btn5.Text == "O" && Btn8.Text == "O") ||
+            (Btn3.Text == "O" && Btn6.Text == "O" && Btn9.Text == "O") ||
+            (Btn1.Text == "O" && Btn5.Text == "O" && Btn9.Text == "O") || //Diagonal
+            (Btn3.Text == "O" && Btn5.Text == "O" && Btn7.Text == "O"))
             {
                 MessageBox.Show("Computer wins");
                 computerWins++;
@@ -104,7 +104,22 @@ namespace Tic_Tac_Toe
                 lblScore2.Text = "Computer wins-" + computerWins;
 
             }
+        counter = counter + 1;
+            if (counter == 9)
+            {
+                MessageBox.Show("Draw");
+                counter = 0;
+            }
         }
+        private void WON()
+        {
+            foreach (Control x in this.Controls)
+                if (x is Button && x.Tag == "play")
+                {
+                    ((Button)x).Enabled = false;
+                    ((Button)x).BackColor = default(Color);
 
+                }
+        }
     }
 }
