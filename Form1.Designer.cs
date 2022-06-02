d998f159383f485e638970fa9b37271ab12bde29
Dan Gabriel Lettac <dan.lettac@gmail.com>
2022-06-02 17:42:22 +0800

    9 buttons added

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace Tic_Tac_Toe
 {
-    partial class Form1
+    partial class formTictactoe
     {
         /// <summary>
         /// Required designer variable.
@@ -28,13 +28,154 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.pnlBoard = new System.Windows.Forms.Panel();
+            this.Btn1 = new System.Windows.Forms.Button();
+            this.Btn2 = new System.Windows.Forms.Button();
+            this.Btn3 = new System.Windows.Forms.Button();
+            this.Btn4 = new System.Windows.Forms.Button();
+            this.Btn5 = new System.Windows.Forms.Button();
+            this.Btn6 = new System.Windows.Forms.Button();
+            this.Btn7 = new System.Windows.Forms.Button();
+            this.Btn8 = new System.Windows.Forms.Button();
+            this.Btn9 = new System.Windows.Forms.Button();
+            this.pnlBoard.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pnlBoard
+            // 
+            this.pnlBoard.BackColor = System.Drawing.Color.Cornsilk;
+            this.pnlBoard.Controls.Add(this.Btn9);
+            this.pnlBoard.Controls.Add(this.Btn8);
+            this.pnlBoard.Controls.Add(this.Btn7);
+            this.pnlBoard.Controls.Add(this.Btn6);
+            this.pnlBoard.Controls.Add(this.Btn5);
+            this.pnlBoard.Controls.Add(this.Btn4);
+            this.pnlBoard.Controls.Add(this.Btn3);
+            this.pnlBoard.Controls.Add(this.Btn2);
+            this.pnlBoard.Controls.Add(this.Btn1);
+            this.pnlBoard.Location = new System.Drawing.Point(17, 20);
+            this.pnlBoard.Name = "pnlBoard";
+            this.pnlBoard.Size = new System.Drawing.Size(636, 469);
+            this.pnlBoard.TabIndex = 0;
+            // 
+            // Btn1
+            // 
+            this.Btn1.BackColor = System.Drawing.Color.FloralWhite;
+            this.Btn1.Location = new System.Drawing.Point(27, 25);
+            this.Btn1.Name = "Btn1";
+            this.Btn1.Size = new System.Drawing.Size(150, 120);
+            this.Btn1.TabIndex = 0;
+            this.Btn1.Tag = "play";
+            this.Btn1.UseVisualStyleBackColor = false;
+            // 
+            // Btn2
+            // 
+            this.Btn2.BackColor = System.Drawing.Color.FloralWhite;
+            this.Btn2.Location = new System.Drawing.Point(242, 25);
+            this.Btn2.Name = "Btn2";
+            this.Btn2.Size = new System.Drawing.Size(150, 120);
+            this.Btn2.TabIndex = 1;
+            this.Btn2.Tag = "play";
+            this.Btn2.UseVisualStyleBackColor = false;
+            // 
+            // Btn3
+            // 
+            this.Btn3.BackColor = System.Drawing.Color.FloralWhite;
+            this.Btn3.Location = new System.Drawing.Point(457, 25);
+            this.Btn3.Name = "Btn3";
+            this.Btn3.Size = new System.Drawing.Size(150, 120);
+            this.Btn3.TabIndex = 2;
+            this.Btn3.Tag = "play";
+            this.Btn3.UseVisualStyleBackColor = false;
+            // 
+            // Btn4
+            // 
+            this.Btn4.BackColor = System.Drawing.Color.FloralWhite;
+            this.Btn4.Location = new System.Drawing.Point(27, 171);
+            this.Btn4.Name = "Btn4";
+            this.Btn4.Size = new System.Drawing.Size(150, 120);
+            this.Btn4.TabIndex = 3;
+            this.Btn4.Tag = "play";
+            this.Btn4.UseVisualStyleBackColor = false;
+            // 
+            // Btn5
+            // 
+            this.Btn5.BackColor = System.Drawing.Color.FloralWhite;
+            this.Btn5.Location = new System.Drawing.Point(242, 171);
+            this.Btn5.Name = "Btn5";
+            this.Btn5.Size = new System.Drawing.Size(150, 120);
+            this.Btn5.TabIndex = 4;
+            this.Btn5.Tag = "play";
+            this.Btn5.UseVisualStyleBackColor = false;
+            // 
+            // Btn6
+            // 
+            this.Btn6.BackColor = System.Drawing.Color.FloralWhite;
+            this.Btn6.Location = new System.Drawing.Point(457, 171);
+            this.Btn6.Name = "Btn6";
+            this.Btn6.Size = new System.Drawing.Size(150, 120);
+            this.Btn6.TabIndex = 5;
+            this.Btn6.Tag = "play";
+            this.Btn6.UseVisualStyleBackColor = false;
+            // 
+            // Btn7
+            // 
+            this.Btn7.BackColor = System.Drawing.Color.FloralWhite;
+            this.Btn7.Location = new System.Drawing.Point(27, 321);
+            this.Btn7.Name = "Btn7";
+            this.Btn7.Size = new System.Drawing.Size(150, 120);
+            this.Btn7.TabIndex = 6;
+            this.Btn7.Tag = "play";
+            this.Btn7.UseVisualStyleBackColor = false;
+            // 
+            // Btn8
+            // 
+            this.Btn8.BackColor = System.Drawing.Color.FloralWhite;
+            this.Btn8.Location = new System.Drawing.Point(242, 321);
+            this.Btn8.Name = "Btn8";
+            this.Btn8.Size = new System.Drawing.Size(150, 120);
+            this.Btn8.TabIndex = 7;
+            this.Btn8.Tag = "play";
+            this.Btn8.UseVisualStyleBackColor = false;
+            // 
+            // Btn9
+            // 
+            this.Btn9.BackColor = System.Drawing.Color.FloralWhite;
+            this.Btn9.Location = new System.Drawing.Point(457, 321);
+            this.Btn9.Name = "Btn9";
+            this.Btn9.Size = new System.Drawing.Size(150, 120);
+            this.Btn9.TabIndex = 8;
+            this.Btn9.Tag = "play";
+            this.Btn9.UseVisualStyleBackColor = false;
+            // 
+            // formTictactoe
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.BackgroundImage = global::Tic_Tac_Toe.Properties.Resources.bgtictac;
+            this.ClientSize = new System.Drawing.Size(956, 585);
+            this.Controls.Add(this.pnlBoard);
+            this.Name = "formTictactoe";
+            this.ShowIcon = false;
+            this.Tag = "play";
+            this.Text = "Versus Bot";
+            this.pnlBoard.ResumeLayout(false);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel pnlBoard;
+        private System.Windows.Forms.Button Btn9;
+        private System.Windows.Forms.Button Btn8;
+        private System.Windows.Forms.Button Btn7;
+        private System.Windows.Forms.Button Btn6;
+        private System.Windows.Forms.Button Btn5;
+        private System.Windows.Forms.Button Btn4;
+        private System.Windows.Forms.Button Btn3;
+        private System.Windows.Forms.Button Btn2;
+        private System.Windows.Forms.Button Btn1;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace Tic_Tac_Toe
 {
-    public partial class Form1 : Form
+    public partial class formTictactoe : Form
     {
-        public Form1()
+        public formTictactoe()
         {
             InitializeComponent();
         }
